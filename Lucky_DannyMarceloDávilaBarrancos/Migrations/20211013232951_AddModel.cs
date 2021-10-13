6cb8a0617c8bff9301699ddf971865a631ea8305
Marcelo Dávila <a2020110679@estudiantes.upsa.edu.bo>
2021-10-13 19:30:41 -0400

    Migrations Implementado

---
+++ Lucky_DannyMarceloDávilaBarrancos/Migrations/20211013232951_AddModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lucky_DannyMarceloDávilaBarrancos.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lucky",
+                columns: table => new
+                {
+                    SuerteID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Detalle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lucky", x => x.SuerteID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lucky");
+        }
+    }
+}
