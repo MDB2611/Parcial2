651ced805f404cc9e6ec0739068b040b22880c62
Marcelo Dávila <a2020110679@estudiantes.upsa.edu.bo>
2021-10-13 19:24:19 -0400

    AppDbContext creado

---
+++ Lucky_DannyMarceloDávilaBarrancos/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Lucky_DannyMarceloDávilaBarrancos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lucky_DannyMarceloDávilaBarrancos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Lucky> Lucky { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
