53b28708de3b5141bcbf7213c8076f1b287a5908
Marcelo Dávila <a2020110679@estudiantes.upsa.edu.bo>
2021-10-13 19:19:03 -0400

    Modelo Lucky Creado

--- Lucky_DannyMarceloDávilaBarrancos/Models/Lucky.cs
+++ Lucky_DannyMarceloDávilaBarrancos/Models/Lucky.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace Lucky_DannyMarceloDávilaBarrancos.Models
 {
     public class Lucky
     {
+        [Key]
+        public string SuerteID { get; set; }
+        [Display(Name = "Suerte: ")]
+        [Required]
+        [Url]
+        public string Detalle { get; set; }
+        [Display(Name = "Foto de la Suerte:")]
+        public string Imagen { get; set; }
+
     }
 }
